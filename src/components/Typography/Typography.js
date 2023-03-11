50bfd8b0f07a1bd063fc333e415d45b54fb2d11f
Sevilla <JM.Sevilla@towerswatson.com>
2023-03-11 12:35:05 +0800

    day2/development_v1

---
+++ src/components/Typography/Typography.js
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+
+export const ControlledTypography = (props) => {
+    const { variant, display, isGutterBottom = false, text, style } = props
+
+    return (
+        <>
+            <Typography
+            variant={variant}
+            display={display}
+            gutterBottom={isGutterBottom}
+            style={style}
+            >
+                {text}
+            </Typography>
+        </>
+    )
+}
